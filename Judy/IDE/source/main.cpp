d4760f907780d442be50fd90773973292acebf7d
Alprog <alprog@hotmail.com>
2014-06-04 23:23:30 +0300

    rebuild exe

--- Judy/IDE/source/main.cpp
+++ Judy/IDE/source/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     QWidget window;
 
     window.resize(250, 150);
-    window.setWindowTitle("Simple example");
+    window.setWindowTitle("Judy");
     window.show();
 
     return app.exec();
