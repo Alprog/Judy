9dc930724fcb14ec1679b2a79d03769b92d8b47e
Alprog <alprog@hotmail.com>
2014-07-28 23:19:39 +0300

    debug lua mess

---
+++ Judy/IDE/luascript.lua
@@ -0,0 +1,9 @@
+
+
+function myluafunction(x)
+	x = x + 1
+	x = x + 2
+	return x + cppvar;
+end
+
+myluafunction(3)
--- Judy/Player/source/main.cpp
+++ Judy/Player/source/main.cpp
@@ -10,7 +10,6 @@ extern "C"
     #include "lua.h"
     #include "lualib.h"
     #include "lauxlib.h"
-
 }
 
 int main(int argc, char *argv[])
