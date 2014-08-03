f2b3ecb45b786aa5d961fa04ad4784dcaddc90c6
Alprog <alprog@hotmail.com>
2014-08-03 20:36:50 +0300

    remove src folder

--- src/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd $(dirname "$(readlink -f "$0")")
-
-gcc Main.cpp -o test `pkg-config --libs --cflags gtk+-3.0` -I "scintilla/include" -L "scintilla/bin" "scintilla/bin/scintilla.a"
-echo 'done'
-read
