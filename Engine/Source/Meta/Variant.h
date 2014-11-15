077a5d348597f1ef83288f3e9226679917371707
Alprog <alprog@hotmail.com>
2014-11-15 23:06:22 +0200

    alternative implementation for variant

--- Engine/Source/Meta/Variant.h
+++ Engine/Source/Meta/Variant.h
@@ -4,65 +4,153 @@
 #include "memory.h"
 #include "stdio.h"
 
+struct IVariantData
+{
+    virtual ~IVariantData()
+    {
+    }
+
+    virtual IVariantData* clone() = 0;
+};
+
+template <typename T>
+struct VariantData : public IVariantData
+{
+    T data;
+
+    VariantData(T const& data)
+        : data(data)
+    {
+    }
+
+    virtual IVariantData* clone() override
+    {
+        return new VariantData<T>(data);
+    }
+};
+
 class Variant
 {
 public:
     static const Variant empty;
 
+    IVariantData* p;
+
     Variant(void)
-        : size{ 0 }
-        , data{ nullptr }
+        : p {nullptr}
     {
     }
 
     template <typename T>
     Variant(T const& value)
     {
-        size = sizeof(value);
-        data = new char[size];
-        memcpy(data, &value, size);
+        p = new VariantData<T>(value);
     }
 
     Variant(const Variant& v)
     {
-        size = v.size;
-        data = new char[size];
-        memcpy(data, v.data, size);
+        if (v.p == nullptr)
+        {
+            p = nullptr;
+        }
+        else
+        {
+            p = v.p->clone();
+        }
     }
 
     ~Variant()
     {
-        if (data != nullptr)
+        if (p != nullptr)
         {
-            delete[] data;
+            delete p;
         }
     }
 
     template <typename T>
     Variant& operator=(T const& value)
     {
-        if (data != nullptr)
+        if (p != nullptr)
         {
-            delete[] data;
+            delete p;
         }
-        size = sizeof(value);
-        data = new char[size];
-        memcpy(data, &value, size);
+        p = new VariantData<T>(value);
         return *(this);
     }
 
     template <typename Type>
     inline Type as()
     {
-        return *(Type*)data;
+        return ((VariantData<Type>*)p)->data;
     }
 
     template <typename Type>
     operator Type()
     {
-        return *(Type*)data;
+        return ((VariantData<Type>*)p)->data;
     }
-
-    size_t size;
-    void* data;
 };
+
+//class Variant
+//{
+//public:
+//    static const Variant empty;
+
+//    Variant(void)
+//        : size{ 0 }
+//        , data{ nullptr }
+//    {
+//    }
+
+//    template <typename T>
+//    Variant(T const& value)
+//    {
+//        size = sizeof(value);
+//        data = new char[size];
+//        memcpy(data, &value, size);
+//    }
+
+//    Variant(const Variant& v)
+//    {
+//        size = v.size;
+//        data = new char[size];
+//        memcpy(data, v.data, size);
+//    }
+
+//    ~Variant()
+//    {
+//        if (data != nullptr)
+//        {
+//            delete[] data;
+//        }
+//    }
+
+//    template <typename T>
+//    Variant& operator=(T const& value)
+//    {
+//        if (data != nullptr)
+//        {
+//            delete[] data;
+//        }
+//        size = sizeof(value);
+
+//        data = new char[size];
+//        memcpy(data, &value, size);
+//        return *(this);
+//    }
+
+//    template <typename Type>
+//    inline Type as()
+//    {
+//        return *(Type*)data;
+//    }
+
+//    template <typename Type>
+//    operator Type()
+//    {
+//        return *(Type*)data;
+//    }
+
+//    size_t size;
+//    void* data;
+//};
