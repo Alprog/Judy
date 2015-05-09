0e7e2ba95657173f4e33bc027ecc1d996171f5d3
Alprog <alprog@hotmail.com>
2015-05-09 19:37:17 +0200

    Singleton

---
+++ Engine/Source/Singleton.h
@@ -0,0 +1,17 @@
+
+
+template <typename T>
+class Singleton
+{
+public:
+    inline static T* const Instance()
+    {
+        return &instance;
+    }
+
+private:
+    static T instance;
+};
+
+template <typename T>
+T Singleton<T>::instance;
