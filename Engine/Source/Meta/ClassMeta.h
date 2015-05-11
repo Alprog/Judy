e0ddc589ed4dc8c44e9171387b418aaec5b5782b
Alprog <alprog@hotmail.com>
2015-05-11 22:13:33 +0200

    Template Specialization

---
+++ Engine/Source/Meta/ClassMeta.h
@@ -0,0 +1,41 @@
+
+#pragma once
+
+#include "ITypeMeta.h"
+#include "Sfinae.h"
+
+class ClassMetaBase : public ITypeMeta
+{
+    bool isPointer() override { return false; }
+    bool isVector() override { return false; }
+    bool isClass() override { return true; }
+};
+
+template <typename T>
+class TypeMeta<T, typename enable_class<T>::type> : public ClassMetaBase
+{
+public:
+    Any CreateOnStack() override { return T(); }
+    Any CreateOnHeap() override { return new T(); }
+
+    virtual Any Dereferencing(Any& object) override
+    {
+        return *(object.as<T*>());
+    }
+
+    virtual Any MakePointerTo(Any& object) override
+    {
+        return &(object.as<T>());
+    }
+
+    virtual ITypeMeta* PointeeTypeMeta() override
+    {
+        throw new std::exception();
+    }
+
+    template <typename... Types>
+    inline static T* New(Types... args)
+    {
+        return new T(args...);
+    }
+};
