54e2767a255af73bf08ccf0b99d20edf181efd74
Alprog <alprog@hotmail.com>
2015-10-05 21:48:04 +0200

    remove comments

--- Engine/Source/Containers/List.h
+++ Engine/Source/Containers/List.h
@@ -62,17 +62,3 @@ private:
         return list;
     }
 };
-
-
-//auto at = static_cast<typename VT::reference (VT::*)(typename VT::size_type)>(&VT::at);
-//auto push_back = static_cast<void (VT::*)(const typename VT::value_type&)>(&VT::push_back);
-
-//ClassDefiner<VT> a = ClassDefiner<VT>(this, name.c_str())
-//    .template valueType<T>()
-//    .constructor()
-//    .function("toAnyVector", &toAnyVector<VT>)
-//    .function("fromAnyVector", &anyVectorToVector<T>)
-//    .method("size", &VT::size)
-//    .method("at", at)
-//    .method("push_back", push_back)
-//;
